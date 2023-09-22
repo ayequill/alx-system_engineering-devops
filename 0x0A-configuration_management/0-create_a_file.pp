5a589065445fd9a1c52f1af22d47577a08f695b5
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-09-22 13:48:12 +0000

    Fixed mode

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,7 @@
 # Create a file in /tmp/myschool with the following content
 file { '/tmp/myschool':
   ensure  => 'file',
-  mode    => '744',
+  mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
   content => 'I love Puppet',

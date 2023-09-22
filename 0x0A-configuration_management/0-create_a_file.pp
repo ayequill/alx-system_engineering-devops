d36be773a853e97e8bb4bb6e7954f9587c6aaa6f
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-09-22 13:44:50 +0000

    Fixed linting issues

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # Create a file in /tmp/myschool with the following content
 file { '/tmp/myschool':
   ensure  => 'file',
-  mode => '0744',
-  owner => 'www-data',
-  group => 'www-data',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
   content => 'I love puppet',
 }

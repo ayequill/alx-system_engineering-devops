ac967bcb02aed69f8375df309a028795fa72cb8d
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-09-22 13:50:30 +0000

    Added path as attribute

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,6 +1,7 @@
 # Create a file in /tmp/myschool with the following content
 file { '/tmp/myschool':
   ensure  => 'file',
+  path    => '/tmp/myschool',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',

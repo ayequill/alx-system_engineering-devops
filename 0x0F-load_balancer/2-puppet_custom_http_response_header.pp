1b6a2a6368fa08e2fca95ed750cb83489199997a
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-10-03 05:53:22 +0000

    Merge branch 'main' of github.com:ayequill/alx-system_engineering-devops

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -9,7 +9,6 @@ package { 'nginx':
 	require => Exec['update_system']
 }
 
-
 file { 'index.html':
     ensure  => 'file',
     path    => '/var/www/html/index.html',
@@ -17,38 +16,18 @@ file { 'index.html':
     require => Package['nginx'],
 }
 
-file { 'default':
-  ensure  => 'file',
+file_line { 'Creating header':
+  ensure  => present,
   path    => '/etc/nginx/sites-available/default',
-  content => "server {
-	listen 80 default_server;
-	listen [::]:80 default_server;
-	root /var/www/html;
-
-	# Add index.php to the list if you are using PHP
-	index index.html index.htm index.nginx-debian.html;
-
-	server_name _;
-	
-	add_header X-Served-By \$hostname;
-
-	location /redirect_me {
-		return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
-	}
-
-	location / {
-		# First attempt to serve request as file, then
-		# as directory, then fall back to displaying a 404.
-		try_files \$uri \$uri/ =404;
-	}
-}",
-  require => Package['nginx'],
-  notify  => Service['nginx'],
+  line    => "\tadd_header X-Served-By ${hostname};",
+  after   => 'server_name _;',
+  require => Service['nginx'],
+  notigy  => Service['nginx'],
 }
 
 # Start nginx
 service { 'nginx':
-  ensure  => running,
-  enable  => true,
+  ensure => running,
+  enable => true,
 # require => File['default'],
 }

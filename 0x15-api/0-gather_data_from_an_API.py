a24c22e6ea03c7f003625bf83c76ef6c51725ca9
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-10-23 20:05:01 +0000

    Debugging

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,34 +1,32 @@
 #!/usr/bin/python3
-""" Requests and displays todos of a user """
+""""
+Script that, using this REST API,
+for a given employee ID, returns information
+about his/her TODOs list progress.
+"""
 from requests import get
-from sys import argv
+import sys
+
+
+def user_data_from_api(userId):
+    """ """
+    user = get('https://jsonplaceholder.typicode.com/users/{}'.format(userId))
+    name = user.json().get('name')
+    response = get('https://jsonplaceholder.typicode.com/users/{}/todos'
+                   .format(userId))
+    data = response.json()
+
+    total_task = len(data)
+    completed_task = 0
+    for task in data:
+        if task.get('completed') is True:
+            completed_task += 1
+    print('Employee {} is done with tasks({}/{}):'
+          .format(name, completed_task, total_task))
+    for task in data:
+        if task.get('completed') is True:
+            print("\t {}".format(task.get('title')))
+
 
 if __name__ == "__main__":
-    user_id = int(argv[1])
-
-    def get_user(id):
-        """ Gets th username """
-        res = get(f'https://jsonplaceholder.typicode.com/users/{id}')
-        return res.json().get('name')
-
-    def get_todo(id):
-        """ Returns data from todo response """
-        res = get('https://jsonplaceholder.typicode.com/todos')
-        user_todos = list(filter(lambda todo: todo.get('userId') == id,
-                                 res.json()))
-        completed = list(filter(lambda todo: todo.get('completed') is True,
-                                user_todos))
-        return {
-            'len_comp': len(completed),
-            'completed': completed,
-            'len_all': len(user_todos)
-        }
-
-    print("Employee {user} is done with tasks({len_comp}/{len_all}):".format(
-        user=get_user(user_id),
-        len_comp=get_todo(user_id).get('len_comp'),
-        len_all=get_todo(user_id).get('len_all')
-    ))
-
-    for todo in get_todo(user_id).get('completed'):
-        print(f"\t {todo['title']}")
+    user_data_from_api(sys.argv[1])

bf43fe53fae821e80d8cdbc53a3cf780539e8e19
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-10-23 18:32:43 +0000

    Refactor

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,30 +3,29 @@
 from requests import get
 from sys import argv
 
-user_id = int(argv[1])
 
+if __name__ == "__main__":
+    user_id = int(argv[1])
 
-def get_user(id):
-    """ Gets th username """
-    res = get(f'https://jsonplaceholder.typicode.com/users/{id}')
-    return res.json().get('name')
+    def get_user(id):
+        """ Gets th username """
+        res = get(f'https://jsonplaceholder.typicode.com/users/{id}')
+        return res.json().get('name')
 
+    def get_todo(id):
+        """ Returns data from todo response """
+        res = get('https://jsonplaceholder.typicode.com/todos')
+        user_todos = list(filter(lambda todo: todo.get('userId') == id,
+                                 res.json()))
+        completed = list(filter(lambda todo: todo.get('completed') is True,
+                                user_todos))
+        return {
+            'len_comp': len(completed),
+            'completed': completed,
+            'len_all': len(user_todos)
+        }
+    print(f"Employee {get_user(user_id)} is done with \
+    ({get_todo(user_id).get('len_comp')}/{get_todo(user_id).get('len_all')}):")
 
-def get_todo(id):
-    """ Returns data from todo response """
-    res = get('https://jsonplaceholder.typicode.com/todos')
-    user_todos = list(filter(lambda todo: todo['userId'] == id, res.json()))
-    completed = list(filter(lambda todo: todo['completed'] is True,
-                            user_todos))
-    return {
-        'len_comp': len(completed),
-        'completed': completed,
-        'len_all': len(user_todos)
-    }
-
-
-print(f"Employee {get_user(user_id)} is done with \
-({get_todo(user_id)['len_comp']}/{get_todo(user_id)['len_all']}):")
-
-for todo in get_todo(user_id)['completed']:
-    print(f"\t {todo['title']}")
+    for todo in get_todo(user_id).get('completed'):
+        print(f"\t {todo['title']}")

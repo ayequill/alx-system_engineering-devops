2830b7c058e10a6eb467918ffbde9eb0971ab5ec
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-11-07 18:43:30 +0000

    Refactored

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
 """ Queries and return the number of subscribers in sub reddit """
+from requests import get
 
-if __name__ == "__main__":
-    from requests import get
 
-    def number_of_subscribers(subreddit):
-        """ Queries the reddit API"""
-        url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-        res = get(url, headers={'User-Agent': 'Mozilla/5.0'})
-        if res.ok:
-            data = res.json()
-            if data.get('data').get('subscribers'):
-                return int(data['data']['subscribers'])
-            else:
-                return 0
+def number_of_subscribers(subreddit):
+    """ Queries the reddit API"""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    res = get(url, headers={'User-Agent': 'Mozilla/5.0'})
+    if res.ok:
+        data = res.json()
+        if data.get('data').get('subscribers'):
+            return int(data['data']['subscribers'])
         else:
             return 0
+    else:
+        return 0
